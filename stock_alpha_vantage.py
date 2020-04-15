0c24980776ec9ab1ee69e8672fe7c3c5115f8b65
kap1147 <joshua@theyardapp.com>
2020-04-15 15:52:35 -0400

    add excel support

--- stock_alpha_vantage.py
+++ stock_alpha_vantage.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 import datetime
 from api_key import api_key
 
-####
-# Work in progress
+# TODO
+# create a queryset method
 
 
 class StockData:
@@ -15,42 +15,51 @@ class StockData:
         self.api_key = api_key
         self.df = {}
 
+   # Add indicators to dataframe
     def indicator(self):
-        # calculate total pv, total volume and VMAP
-        self.df['6. pv total'] = (
-            ((self.df['2. high'] + self.df['3. low'] + self.df['4. close']) / 3) * self.df['5. volume']).cumsum()
-        self.df['7. volume total'] = self.df['5. volume'].cumsum()
-        self.df['8. VWAP'] = self.df['6. pv total'] / \
-            self.df['7. volume total']
+        # VWAP
+        # TODO
+        # take in a list and loop through
+        if True:
+            self.df['6. pv total'] = (
+                ((self.df['2. high'] + self.df['3. low'] + self.df['4. close']) / 3) * self.df['5. volume']).cumsum()
+            self.df['7. volume total'] = self.df['5. volume'].cumsum()
+            self.df['8. VWAP'] = self.df['6. pv total'] / \
+                self.df['7. volume total']
 
-    def clean_data(self):
-        # return yesterday's data
+    def set_queryset(self):
+        # TODO
+        # a better query method q:
         yesterday = pd.Timestamp(
             datetime.date.today() - datetime.timedelta(days=1))
         self.df = self.df[self.df.index > yesterday
                           ].sort_index(ascending=False)
-        print('data cleaned!')
 
-    def TimeSeries(self, plot=False to_excel=False):
+    def TimeSeries(self, plot=False, to_excel=False):
         ts = TimeSeries(key=self.api_key, output_format='pandas')
         self.df, meta_data = ts.get_intraday(
             symbol='MSFT', interval='1min', outputsize='full')
 
         print('dataframe created')
-        self.clean_data()
+        self.set_queryset()
         self.indicator()
         self.df = self.df.drop(['2. high', '3. low', '5. volume',
                                 '6. pv total', '7. volume total'], axis=1)
 
         if plot == True:
+            # create plot
+            # a lot to do here
             self.df.plot()
             plt.title('Intraday Time Series (1 min)')
             plt.grid()
             plt.show()
 
         if to_excel == True:
-            self.df.to_excel("intraday {}.xlsx".format())
+            # create excel file
+            # TODO
+            # format a better name
+            self.df.to_excel("intraday.xlsx")
 
 
 df = StockData()
-df.TimeSeries()
+df.TimeSeries(plot=True, to_excel=True)
